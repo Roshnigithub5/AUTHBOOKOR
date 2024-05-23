bd59c2f36f715f505bd75190ef3e19dbae1ace87
DeltaStudent <new@gmail.com>
2024-05-23 18:04:11 +0530

    Added successfully

---
+++ public/js/nav.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const toggler = document.querySelector(".navbar-toggler");
+    toggler.addEventListener('click', function() {
+        toggler.classList.toggle("bg-white-toggler");
+    });
+});
